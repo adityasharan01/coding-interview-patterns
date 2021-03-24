2290b931f28b9d3c0ac058d4a2ef77b1f932f5c8
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-03-25 00:03:28 +0530

    Update Problem10_Container With Most Water.java

--- Blind 75 Leetcode/Problem10_Container With Most Water.java
+++ Blind 75 Leetcode/Problem10_Container With Most Water.java
@@ -17,3 +17,47 @@ Example 4:
 
 Input: height = [1,2,1]
 Output: 2
+
+  Algorithm-1 (Brute force)
+A simple brute force algorithm is what you can start with.
+
+public int maxArea(int[] height) {
+	int max = 0;
+	int l = height.length;
+	
+	for (int i=0; i<l; i++) {
+		for (int j=i+1; j<l; j++) {
+			int area = (j-i) * Math.min(height[i], height[j]);
+			if (area > max) {
+				max = area;
+			}
+		}
+	}
+	
+	return max;
+}
+Algorithm-2
+Lets think of optimizing this computation. What if we start with max range, and move either left or right. Since, lower height determines the area. And, we can move greedily towards point with high height. And, compare the area.
+
+We can start with left and right pointer, calculate area. Then we can move towards point with higher height. i.e. the side which is having less height, we should move that pointer. Example: If left pointer value is less, move it right. Similarly, if right pointer value is less than right, move it left.
+
+public int maxArea(int[] height) {
+	int l = height.length;
+	int result = 0;
+	int i=0;
+	int j = l-1;
+
+	while (i < j) {
+		int area = (j-i) * Math.min(height[i], height[j]);
+		if (result < area) {
+			result = area;
+		}
+
+		if (height[i] < height[j]) {
+			i++;
+		}
+		else j--;
+	}
+
+	return result;
+}
