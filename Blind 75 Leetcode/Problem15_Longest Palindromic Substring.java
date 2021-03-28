b697684a350ad4aa2eaac0bf65c08e4614419df7
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-03-28 12:22:36 +0530

    Update Problem15_Longest Palindromic Substring.java

--- Blind 75 Leetcode/Problem15_Longest Palindromic Substring.java
+++ Blind 75 Leetcode/Problem15_Longest Palindromic Substring.java
@@ -27,6 +27,47 @@ Constraints:
 s consist of only digits and English letters (lower-case and/or upper-case),
 
 
+
+
+EASY APPROACH 
+class Solution {
+
+	public String longestPalindrome(String s) {
+
+		int start = 0;
+		int end = 0;
+
+		for(int i = 0; i < s.length(); i++) {
+
+			int lenOdd = lenOfPalidrome(s, i, i);
+			int lenEven = lenOfPalidrome(s, i, i+1);
+
+			int maxCurrentLen = Math.max(lenOdd, lenEven);
+
+			if(maxCurrentLen > end-start+1) {
+
+				start = i - (maxCurrentLen-1) / 2;
+				end = i + maxCurrentLen / 2;
+
+			}
+		}
+
+		return s.substring(start, end+1);
+	}
+
+
+	public int lenOfPalidrome(String s, int start, int end) {
+
+		while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+			start--;
+			end++;
+		}
+
+		return end - start - 1;  
+	}
+
+}
+
 1. Dynamic Programming
 
 Let s be the input string, i and j are two indices of the string. Define a 2-dimension array "table" and let table[i][j] denote whether a substring from i to j is palindrome.
