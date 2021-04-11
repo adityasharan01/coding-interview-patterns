0ea4ec5ab742ac7a06ae2bd8a9991a9b15020fe1
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-04-11 20:43:32 +0530

    Create BFS and DFS iterative approach.java

---
+++ Blind 75 Leetcode/Trees/BFS and DFS iterative approach.java
@@ -0,0 +1,55 @@
+This is the iterative version of finding the depth. The recursive version is trivial, so expect the interviewer to ask for the iterative version. I used two stacks for the dfs one and a queue for the level-order traversal one. Level order one is faster.
+
+DFS
+
+public int maxDepth(TreeNode root) {
+    if(root == null) {
+        return 0;
+    }
+    
+    Stack<TreeNode> stack = new Stack<>();
+    Stack<Integer> value = new Stack<>();
+    stack.push(root);
+    value.push(1);
+    int max = 0;
+    while(!stack.isEmpty()) {
+        TreeNode node = stack.pop();
+        int temp = value.pop();
+        max = Math.max(temp, max);
+        if(node.left != null) {
+            stack.push(node.left);
+            value.push(temp+1);
+        }
+        if(node.right != null) {
+            stack.push(node.right);
+            value.push(temp+1);
+        }
+    }
+    return max;
+}
+// 7ms
+BFS
+
+public int maxDepth(TreeNode root) {
+    if(root == null) {
+        return 0;
+    }
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    int count = 0;
+    while(!queue.isEmpty()) {
+        int size = queue.size();
+        while(size-- > 0) {
+            TreeNode node = queue.poll();
+            if(node.left != null) {
+                queue.offer(node.left);
+            }
+            if(node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        count++;
+    }
+    return count;
+}
+// 3ms
