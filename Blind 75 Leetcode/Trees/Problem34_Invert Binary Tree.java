834860cd23115a805491576757a86736007350c7
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-04-13 21:09:59 +0530

    Create Problem34_Invert Binary Tree.java

---
+++ Blind 75 Leetcode/Trees/Problem34_Invert Binary Tree.java
@@ -0,0 +1,151 @@
+Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ Approach: 
+Idea is to traverse the tree in a preorder fashion and for every node encountered, swap its and right child before recursively inverting its left and right subtree.
+We can also traverse tree in postorder fashion.
+  Java Solution:
+  class Node
+{
+    int data;
+    Node left = null, right = null;
+ 
+    Node(int data) {
+        this.data = data;
+    }
+}
+ 
+class Main
+{
+    // Function to perform preorder traversal on a given binary tree
+    public static void preorder(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+ 
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+ 
+    // Utility function to swap left subtree with right subtree
+    public static void swap(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+ 
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+    }
+ 
+    // Function to invert a given binary tree using preorder traversal
+    public static void invertBinaryTree(Node root)
+    {
+        // base case: if the tree is empty
+        if (root == null) {
+            return;
+        }
+ 
+        // swap left subtree with right subtree
+        swap(root);
+ 
+        // invert left subtree
+        invertBinaryTree(root.left);
+ 
+        // invert right subtree
+        invertBinaryTree(root.right);
+    }
+ 
+    public static void main(String[] args)
+    {
+        /* Construct the following tree
+                  1
+                /   \
+               /     \
+              2       3
+             / \     / \
+            4   5   6   7
+        */
+ 
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+ 
+        invertBinaryTree(root);
+        preorder(root);
+    }
+}
+  
+ Python Solution :
+ class Node:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+ 
+ 
+# Function to perform preorder traversal on a given binary tree
+def preorder(root):
+ 
+    if root is None:
+        return
+ 
+    print(root.data, end=' ')
+    preorder(root.left)
+    preorder(root.right)
+ 
+ 
+# Utility function to swap left subtree with right subtree
+def swap(root):
+ 
+    if root is None:
+        return
+ 
+    temp = root.left
+    root.left = root.right
+    root.right = temp
+ 
+ 
+# Function to invert a given binary tree using preorder traversal
+def invertBinaryTree(root):
+ 
+    # base case: if the tree is empty
+    if root is None:
+        return
+ 
+    # swap left subtree with right subtree
+    swap(root)
+ 
+    # invert left subtree
+    invertBinaryTree(root.left)
+ 
+    # invert right subtree
+    invertBinaryTree(root.right)
