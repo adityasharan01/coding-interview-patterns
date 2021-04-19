670f970bca592604525e15042fbbdb309e48c4e5
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-04-20 00:03:52 +0530

    Create Problem44_Implement Trie (Prefix Tree).java

---
+++ Blind 75 Leetcode/Trees/Problem44_Implement Trie (Prefix Tree).java
@@ -0,0 +1,80 @@
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+ 
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+ 
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+ //////////////////////////////////////
+  SOLUTION IS VERY WELL WRITTEN IN LEETCODE 
+  HEAD OVER TO BELOW LINK:-
+  https://leetcode.com/problems/implement-trie-prefix-tree/
+  
+  
+  //////////////////////////////////
+  Python Approach 
+  class Trie:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = {}
+        self.stop_character = '__s'
+        
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for letter in word:
+            node = node.setdefault(letter, {})
+        node[self.stop_character] = True
+        
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for letter in word:
+            if letter not in node:
+                return False
+            node = node[letter]
+        return node.get(self.stop_character, False)
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for letter in prefix:
+            if letter not in node:
+                return False
+            node = node[letter]
+        return True
