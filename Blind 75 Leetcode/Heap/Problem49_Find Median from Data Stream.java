6aa8d93c8129acc95cf105f3e50f904d85493ab9
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-04-23 16:27:05 +0530

    Create Problem49_Find Median from Data Stream.java

---
+++ Blind 75 Leetcode/Heap/Problem49_Find Median from Data Stream.java
@@ -0,0 +1,94 @@
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+  
+  
+  
+  
+  
+  class MedianFinder {
+    
+    private List<Integer> ds = new ArrayList<>();
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+
+    }
+    
+    public void addNum(int num) {
+        if (ds.isEmpty()) {
+            ds.add(num);
+            return;
+        }
+        int position = getInsertPosition(num);
+        ds.add(position, num);
+    }
+    
+    public double findMedian() {
+        if (ds.isEmpty()) {
+            return 0;
+        }
+        if (ds.size() % 2 == 0) {
+            int mid = ds.size() / 2;
+            int preMid = mid - 1;
+            int sum = ds.get(mid) + ds.get(preMid);
+            return (double) sum / 2;
+        } else {
+            int mid = ds.size() / 2;
+            return (double) ds.get(mid);
+        }
+    }
+    
+    private int getInsertPosition(int num) {
+        int lo = 0, hi = ds.size() - 1;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (num == ds.get(mid)) {
+                return mid;
+            }
+            if (ds.get(mid) > num) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}
