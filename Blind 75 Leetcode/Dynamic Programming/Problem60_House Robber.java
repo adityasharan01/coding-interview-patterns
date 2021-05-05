f78bdcbe1467d2cd36ecb69390c8986f9dc34593
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-05-05 23:02:16 +0530

    Create Problem60_House Robber.java

---
+++ Blind 75 Leetcode/Dynamic Programming/Problem60_House Robber.java
@@ -0,0 +1,43 @@
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+  
+  
+  Approach :
+Example Input :         1, 3, 4, 2, 5,  7,  2, 3
+----------------------------------------
+rob:                    1, 3, 5, 5, 10, 12, 12, 15
+not rob:                0, 1, 3, 5, 5,  10, 12, 12
+----------------------------------------
+
+public int rob(int[] num) {
+    int rob = 0; //max monney can get if rob current house
+    int notrob = 0; //max money can get if not rob current house
+    for(int i=0; i<num.length; i++) {
+        int currob = notrob + num[i]; //if rob current value, previous house must not be robbed
+        notrob = Math.max(notrob, rob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
+        rob = currob;
+    }
+    return Math.max(rob, notrob);
+}
