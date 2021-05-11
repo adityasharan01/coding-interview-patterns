70dc73ad0071502b1307049c06072eb18b6a1e05
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-05-12 00:35:16 +0530

    Create Problem70_Longest Consecutive Sequence.java

---
+++ Blind 75 Leetcode/Graph/Problem70_Longest Consecutive Sequence.java
@@ -0,0 +1,76 @@
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 104
+-109 <= nums[i] <= 109
+ 
+
+Follow up: Could you implement the O(n) solution?
+
+We will use HashMap. The key thing is to keep track of the sequence length and store that in the boundary points of the sequence. For example, as a result, for sequence {1, 2, 3, 4, 5}, map.get(1) and map.get(5) should both return 5.
+
+Whenever a new element n is inserted into the map, do two things:
+
+See if n - 1 and n + 1 exist in the map, and if so, it means there is an existing sequence next to n. Variables left and right will be the length of those two sequences, while 0 means there is no sequence and n will be the boundary point later. Store (left + right + 1) as the associated value to key n into the map.
+Use left and right to locate the other end of the sequences to the left and right of n respectively, and replace the value with the new length.
+Everything inside the for loop is O(1) so the total time is O(n). Please comment if you see something wrong. Thanks.
+
+public int longestConsecutive(int[] num) {
+    int res = 0;
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int n : num) {
+        if (!map.containsKey(n)) {
+            int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
+            int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+            // sum: length of the sequence n is in
+            int sum = left + right + 1;
+            map.put(n, sum);
+            
+            // keep track of the max length 
+            res = Math.max(res, sum);
+            
+            // extend the length to the boundary(s)
+            // of the sequence
+            // will do nothing if n has no neighbors
+            map.put(n - left, sum);
+            map.put(n + right, sum);
+        }
+        else {
+            // duplicates
+            continue;
+        }
+    }
+    return res;
+}
+
+
+
+//////////////////////////
+First turn the input into a set of numbers. That takes O(n) and then we can ask in O(1) whether we have a certain number.
+
+Then go through the numbers. If the number x is the start of a streak (i.e., x-1 is not in the set), then test y = x+1, x+2, x+3, ... and stop at the first number y not in the set. The length of the streak is then simply y-x and we update our global best with that. Since we check each streak only once, this is overall O(n). This ran in 44 ms on the OJ, one of the fastest Python submissions.
+
+def longestConsecutive(self, nums):
+    nums = set(nums)
+    best = 0
+    for x in nums:
+        if x - 1 not in nums:
+            y = x + 1
+            while y in nums:
+                y += 1
+            best = max(best, y - x)
+    return best
