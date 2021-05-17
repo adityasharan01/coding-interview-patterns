a2f5fbd7a035171dc74368f7023d5ccbe2895004
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-05-17 16:35:24 +0530

    Create Prblem8.java

---
+++ SDE_Sheet/Day2(Arrays)/Prblem8.java
@@ -0,0 +1,38 @@
+Pascal's Triangle
+  Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+ 
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= numRows <= 30
+//Approach:
+  public class Solution {
+public List<List<Integer>> generate(int numRows)
+{
+	List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+	ArrayList<Integer> row = new ArrayList<Integer>();
+	for(int i=0;i<numRows;i++)
+	{
+		row.add(0, 1);
+		for(int j=1;j<row.size()-1;j++)
+			row.set(j, row.get(j)+row.get(j+1));
+		allrows.add(new ArrayList<Integer>(row));
+	}
+	return allrows;
+	
+}
+}
