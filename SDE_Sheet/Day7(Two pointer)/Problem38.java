121ad590f835db17586c8e4afe4c2e543fd7d4a1
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-05-26 16:28:39 +0530

    Create Problem38.java

---
+++ SDE_Sheet/Day7(Two pointer)/Problem38.java
@@ -0,0 +1,48 @@
+3 sum
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
+
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+ 
+
+Sort the array, iterate through the list, and use another two pointers to approach the target.
+
+public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    for (int i = 0; i + 2 < nums.length; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+            continue;
+        }
+        int j = i + 1, k = nums.length - 1;  
+        int target = -nums[i];
+        while (j < k) {
+            if (nums[j] + nums[k] == target) {
+                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
+                while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
+            } else if (nums[j] + nums[k] > target) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return res;
+}
