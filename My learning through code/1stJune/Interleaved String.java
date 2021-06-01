0445a266e22f2aa4409481ba9a2b760716bd8ba7
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-06-01 21:42:45 +0530

    Update Interleaved String.java

--- My learning through code/1stJune/Interleaved String.java
+++ My learning through code/1stJune/Interleaved String.java
@@ -41,3 +41,40 @@ class Solution {
         return new String(st);   // OBSERVE THIS
     }
 }
+
+Intuition
+Iterate over both strings and appending the char at s0 and then s1 as long as either string has not reach the end. Once it has, we check if the index is equal or greater to s0 length, and if it is we add the remaining of string s1, else we add the remaining of string s0. Note, if both strings are equal we would be adding the remaining empty string and thus effectively not adding at all.
+
+Implementation
+=> Initialize str with StringBuilder to ""
+=> Iterate over s0 and s1 while one of them have not reached the end of the string. In each iteration we append the char at s0 and then s1 to str.
+=> Once we have iterated we append the remaining of s0 or s1 depending on if our indexing variable i is >= to the length of the string.
+=> We then return the str as a String.
+
+Time Complexity
+\mathcal{O}(n)O(n) Time is proportional to the length of s0 or s1, whichever is shorter, as that is how long we are iterating in our loop at maximum.
+
+Space Complexity
+\mathcal{O}(n)O(n) str is proportional to the length of s0 and s1 combined.
+
+import java.util.*;
+
+class Solution {
+    public String solve(String s0, String s1) {
+        StringBuilder str = new StringBuilder("");
+
+        int i = 0;
+
+        while (i < s0.length() && i < s1.length()) {
+            str.append(s0.charAt(i));
+            str.append(s1.charAt(i++));
+        }
+
+        if (i >= s0.length())
+            str.append(s1.substring(i));
+        else
+            str.append(s0.substring(i));
+
+        return str.toString();
+    }
+}
