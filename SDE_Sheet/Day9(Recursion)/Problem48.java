4e18576c6966c398b07c0e4136f8e9379e4d6319
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-06-02 10:43:38 +0530

    Update Problem48.java

--- SDE_Sheet/Day9(Recursion)/Problem48.java
+++ SDE_Sheet/Day9(Recursion)/Problem48.java
@@ -44,3 +44,41 @@ class GFG {
     }
 }
  
+// Iterative Java program to print sums of all
+// possible subsets.
+import java.util.*;
+ 
+class GFG {
+ 
+    // Prints sums of all subsets of array
+    static void subsetSums(int arr[], int n)
+    {
+ 
+        // There are totoal 2^n subsets
+        int total = 1 << n;
+ 
+        // Consider all numbers from 0 to 2^n - 1
+        for (int i = 0; i < total; i++) {
+            int sum = 0;
+ 
+            // Consider binary reprsentation of
+            // current i to decide which elements
+            // to pick.
+            for (int j = 0; j < n; j++)
+                if ((i & (1 << j)) != 0)
+                    sum += arr[j];
+ 
+            // Print sum of picked elements.
+            System.out.print(sum + " ");
+        }
+    }
+ 
+    // Driver code
+    public static void main(String args[])
+    {
+        int arr[] = new int[] { 5, 4, 3 };
+        int n = arr.length;
+ 
+        subsetSums(arr, n);
+    }
+}
