76b483f611ca2dca134adeafb6a8068c88d7ecff
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-06-06 02:20:19 +0530

    Update Reverse Words in a String.java

--- SDE_Sheet/Day15(String)/Reverse Words in a String.java
+++ SDE_Sheet/Day15(String)/Reverse Words in a String.java
@@ -14,6 +14,27 @@ Output: s = “practice of lot a needs coding at good getting”
 import java.util.*;
 import java.io.*;
 import java.lang.*;
+
+// Java program to reverse a string
+// s = input()
+public class ReverseWords
+{
+
+	public static void main(String[] args)
+	{
+		String s[] = "i like this program very much".
+										split(" ");
+		String ans = "";
+		for (int i = s.length - 1; i >= 0; i--)
+		{
+			ans += s[i] + " ";
+		}
+		System.out.println("Reversed String:");
+		System.out.println(ans.substring(0,
+								ans.length() - 1));
+	}
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
   
 class GFG { 
        
