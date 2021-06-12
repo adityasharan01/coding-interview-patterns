7db774bfcb65fffbb82ea94b6ad429cfd25a7dca
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-06-12 14:28:31 +0530

    Create Kth Largest Element in a Stream.java

---
+++ SDE_Sheet/Day22(Mixed Questions)/Kth Largest Element in a Stream.java
@@ -0,0 +1,54 @@
+Kth Largest Element in a Stream
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+int add(int val) Returns the element representing the kth largest element in the stream.
+ 
+
+Example 1:
+
+Input
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+Output
+[null, 4, 5, 5, 8, 8]
+
+Explanation
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3);   // return 4
+kthLargest.add(5);   // return 5
+kthLargest.add(10);  // return 5
+kthLargest.add(9);   // return 8
+kthLargest.add(4);   // return 8
+ 
+
+Constraints:
+
+1 <= k <= 104
+0 <= nums.length <= 104
+-104 <= nums[i] <= 104
+-104 <= val <= 104
+At most 104 calls will be made to add.
+It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+  
+class KthLargest {
+    PriorityQueue<Integer> pq;
+    int k=0;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for(int i: nums) {
+            this.add(i); // call class's own function, instead of repeating yourself
+        }
+    }
+    
+    public int add(int val) {
+        pq.add(val);
+        if(pq.size()>k) { // remove elements ONLY when heap size is too big
+            pq.remove();
+        }
+        return pq.peek();
+    }
+}
