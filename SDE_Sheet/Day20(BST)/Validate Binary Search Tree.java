1ea7e613d4d2ab3d9eb4f8c965e0d052fe132073
Aditya Sharan <40923241+adityasharan01@users.noreply.github.com>
2021-06-12 14:07:53 +0530

    Create Validate Binary Search Tree.java

---
+++ SDE_Sheet/Day20(BST)/Validate Binary Search Tree.java
@@ -0,0 +1,60 @@
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    public boolean validBST(TreeNode root, long minVal, long maxVal){
+        if(root == null) return true;
+        
+        if(root.val >= maxVal || root.val <= minVal) return false;
+        
+        return ((validBST(root.left, minVal, root.val)) && (validBST(root.right, root.val,maxVal)));
+    }
+}
+
+///
+class Solution {
+    TreeNode prev = null;
+    boolean ans = true;
+    public boolean isValidBST(TreeNode root) {
+        inOrder(root);
+        return ans;
+    }
+    public boolean inOrder(TreeNode root){
+        if(root==null)
+            return true;
+        inOrder(root.left);
+        if(prev!=null)
+            ans = ans && prev.val<root.val;
+        prev=root;
+        inOrder(root.right);
+        return true;
+    }
+}
